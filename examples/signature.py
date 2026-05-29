72c47eda46fb0c9b380d7277c0860d1feaed7a78
akabul0us <jeremywokka@proton.me>
2026-05-28 23:03:24 -0600

    combined signature670.py into signature.py (pass -l for little endian)

--- examples/signature.py
+++ examples/signature.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """Extract signature text from config.bin"""
 
 import argparse
@@ -14,20 +15,26 @@ def main():
     parser.add_argument(
         "infile", type=argparse.FileType("rb"), help="Configuration file (config.bin)"
     )
-
+    parser.add_argument(
+            '-l','--littleendian', help='Use --littleendian (-l) for F670', action="store_true"
+    )
     args = parser.parse_args()
-
     infile = args.infile
 
-    header = infile.read(4)
-    if header == b"BAMC":
-        print(f"ERROR: {infile.name} is base64 encoded, please decode and try again.")
-        return 1
-    infile.seek(0)
+    if args.littleendian:
+            zcu.zte.read_header(infile, little_endian=True)
+            sig = zcu.zte.read_signature(infile)
+            print(sig.decode())
+    else:
+        header = infile.read(4)
+        if header == b"BAMC":
+            print(f"ERROR: {infile.name} is base64 encoded, please decode and try again.")
+            return 1
+        infile.seek(0)
 
-    zcu.zte.read_header(infile)
-    signature = zcu.zte.read_signature(infile)
-    print(signature.decode("utf-8"))
+        zcu.zte.read_header(infile)
+        signature = zcu.zte.read_signature(infile)
+        print(signature.decode("utf-8"))
 
 
 if __name__ == "__main__":
--- examples/signature670.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import zcu
-with open('config.bin', 'rb') as f:
-    zcu.zte.read_header(f, little_endian=True)
-    sig = zcu.zte.read_signature(f)
-    print(sig.decode())
-# F670L
